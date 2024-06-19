2b76f4542470ded38f031d848a9d1d229747479e
Admin <nisha.soni@helixtechinc.com>
2024-06-19 13:07:55 -0400

    first commit

---
+++ ems-backend/src/main/java/com/nishasoni/mapper/EmployeeMapper.java
@@ -0,0 +1,26 @@
+package com.nishasoni.mapper;
+
+import com.nishasoni.dto.EmployeeDto;
+import com.nishasoni.entity.Employee;
+
+public class EmployeeMapper {
+
+    public static EmployeeDto mapToEmployeeDto(Employee employee){
+        return new EmployeeDto(
+                employee.getId(),
+                employee.getFirstName(),
+                employee.getLastName(),
+                employee.getEmail(),
+                employee.getDepartment().getId()
+        );
+    }
+
+    public static Employee mapToEmployee(EmployeeDto employeeDto){
+        Employee employee = new Employee();
+        employee.setId(employeeDto.getId());
+        employee.setFirstName(employeeDto.getFirstName());
+        employee.setLastName(employeeDto.getLastName());
+        employee.setEmail(employeeDto.getEmail());
+        return employee;
+    }
+}
---
+++ todo-management/src/main/java/com/nishasoni/repository/RoleRepository.java
@@ -0,0 +1,9 @@
+package com.nishasoni.repository;
+
+import com.nishasoni.entity.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RoleRepository extends JpaRepository<Role, Long> {
+
+    Role findByName(String name);
+}
